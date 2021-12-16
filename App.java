308e10d3616c9e5905a30e005f78050c17ed67d8
JhordP <j.horda.ny@hotmail.com>
2021-12-16 13:58:27 -0400

    Se inserto scanner al Hello

--- App.java
+++ App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("What's your name?");
+        Scanner in = new Scanner(System.in);
+        var name = in.nextLine();
+        System.out.println("Hello, "+name);
+        in.close();
     }
 }
