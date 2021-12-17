ba77c049a75c82d6cdb207c1571359bdec22a8d6
JhordP <j.horda.ny@hotmail.com>
2021-12-16 23:53:00 -0400

    Se agrego pregunta edad.

--- App.java
+++ App.java
@@ -5,7 +5,11 @@ public class App {
         System.out.println("What's your name?");
         Scanner in = new Scanner(System.in);
         var name = in.nextLine();
-        System.out.println("Hello, "+name+".");
+        System.out.println("Hello, "+name+". What's your age?");
+        var edad = in.nextInt();
+
+        System.out.println("User data: \nName: "+name+"\nAge: "+edad);
+
         in.close();
     }
 }
